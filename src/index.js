78608ab217be566421312c61e9b0f76b3953359b
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-11-25 13:50:07 -0800

    added functions from old script

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { scrollIn } from './interactions/scroll-in';
 import { load } from './interactions/load';
 import { initLenis } from './interactions/lenis';
 import { parallax } from './interactions/parallax';
+import Swiper from 'swiper';
+import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', function () {
   // Comment out for production
-  console.log('Local Script');
+  // console.log('Local Script');
   // register gsap plugins if available
   if (gsap.ScrollTrigger !== undefined) {
     gsap.registerPlugin(ScrollTrigger);
@@ -17,7 +19,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   //init lenis library
   const lenis = initLenis();
-  console.log(lenis);
 
   //////////////////////////////
   //Functions from Original JS
@@ -86,6 +87,143 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // ============================================
+  // Project Template Page
+  // ============================================
+  function pageProjectTemplate() {
+    // Project video interaction
+    const projectVideos = $('.project_hero_video_wrap');
+    //return if items aren't found
+    if (projectVideos.length === 0) return;
+    projectVideos.on('click', function () {
+      const video = $(this).find('video')[0];
+
+      // Add the 'active' class only if it's not already present
+      if (!$(this).hasClass('active')) {
+        $(this).addClass('active');
+      }
+
+      // Play the video
+      if (video) {
+        video.play();
+      }
+    });
+
+    // Credits toggle
+    const creditsWrapper = $('.project_hero_credits_wrap');
+    const creditsContent = $('.project_credits_wrap');
+    $('.project_toggle_wrap').on('click', function () {
+      creditsWrapper.toggleClass('active');
+
+      if (creditsWrapper.hasClass('active')) {
+        const scrollHeight = creditsContent.prop('scrollHeight');
+        creditsContent.css('max-height', `${scrollHeight}px`);
+      } else {
+        creditsContent.css('max-height', '7.5em');
+      }
+    });
+  }
+
+  // ============================================
+  // Editor Template Page
+  // ============================================
+  function pageEditorTemplate() {
+    // Lightbox animation
+    const lightboxWrap = $('.editor_lightbox_wrap');
+    const workLinks = $('.work_link');
+    //check if elements are there
+    if (lightboxWrap.length === 0) return;
+
+    workLinks.on('click', function () {
+      // Toggle the lightbox active state
+      lightboxWrap.toggleClass('active');
+
+      if (lightboxWrap.hasClass('active')) {
+        lenis.stop(); // Stop Lenis scrolling when lightbox is active
+
+        // Get the data-editor-id of the clicked item
+        const editorId = $(this).attr('data-editor-id');
+
+        // Find the corresponding slide with the same data-editor-id
+        const correspondingSlide = $('.swiper-slide[data-editor-id="' + editorId + '"]');
+
+        // Get the index of the corresponding slide
+        const slideIndex = correspondingSlide.index();
+
+        // Use Swiper's slideTo method to move to the corresponding slide
+        lightboxSwiper.slideTo(slideIndex);
+      } else {
+        lenis.start(); // Start Lenis scrolling when lightbox is inactive
+      }
+    });
+
+    // Close the overlay when clicking the close icon
+    $('[data-close-icon="overlay"]').on('click', function () {
+      lightboxWrap.removeClass('active');
+      lenis.start(); // Start Lenis when overlay is closed
+    });
+
+    // Close the overlay on pressing the Escape key
+    $(document).on('keydown', function (e) {
+      if (e.key === 'Escape') {
+        lightboxWrap.removeClass('active');
+        lenis.start(); // Start Lenis when Escape key is pressed
+      }
+    });
+
+    // Editor Lightbox Swiper
+    const lightboxSwiper = new Swiper('.swiper.is-editor', {
+      modules: [Navigation, EffectCoverflow],
+      slidesPerView: 'auto',
+      loopedSlides: 3,
+      speed: 500,
+      loop: true,
+      keyboard: true,
+      allowTouchMove: false,
+      grabCursor: false,
+      navigation: {
+        nextEl: '.swiper-next',
+      },
+      effect: 'coverflow',
+      coverflowEffect: {
+        rotate: 0,
+        scale: 0.9,
+        slideShadows: false,
+      },
+    });
+
+    // Play the video of the active slide and show controls
+    const videos = $('.lightbox_visual_video video');
+    const activeVideo = $('.swiper-slide-active .lightbox_visual_video video');
+
+    // Check if there are any videos on the page
+    if (videos.length && activeVideo.length) {
+      // Play the video of the active slide and show controls
+      activeVideo[0].setAttribute('controls', 'true');
+      activeVideo[0].play();
+
+      // Listen for slide change to control video playback
+      lightboxSwiper.on('slideChangeTransitionEnd', function () {
+        const activeVideo = $('.swiper-slide-active .lightbox_visual_video video');
+
+        // Ensure activeVideo exists before attempting to manipulate it
+        if (activeVideo.length) {
+          $(videos).each(function () {
+            this.pause(); // Pause the video
+            this.currentTime = 0; // Reset to the start
+            $(this).removeAttr('controls'); // Optionally remove controls
+          });
+
+          activeVideo[0].setAttribute('controls', 'true');
+          activeVideo[0].play();
+        }
+      });
+    }
+  }
+
+  //////////////////////////////
+  //Unique page-level functions
+
   //////////////////////////////
   //Control Functions on page load
   const gsapInit = function () {
@@ -103,7 +241,10 @@ document.addEventListener('DOMContentLoaded', function () {
         //functional interactions
         load(gsapContext);
         hoverActive(gsapContext);
+        //OG Interactions
         globalNavbar();
+        pageProjectTemplate();
+        pageEditorTemplate();
         //conditional interactions
         if (!reduceMotion) {
           scrollIn(gsapContext);
